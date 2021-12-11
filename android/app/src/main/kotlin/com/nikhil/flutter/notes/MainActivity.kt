f2cd5182b1b15e3f4ac702e0143e28266101f203
Nikhil <nikhill773384@gmail.com>
2021-12-11 15:21:26 +0530

    Minor Fixes

--- android/app/src/main/kotlin/com/nikhil/flutter/notes/MainActivity.kt
+++ android/app/src/main/kotlin/com/nikhil/flutter/notes/MainActivity.kt
@@ -2,6 +2,9 @@ package com.nikhil.flutter.notes
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity : FlutterFragmentActivity() {
+    override fun provideSplashScreen(): SplashScreen = SplashView()
+
 }
