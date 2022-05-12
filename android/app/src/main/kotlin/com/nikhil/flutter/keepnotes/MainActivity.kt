c094684cb4657390fc3dc893700f0dff1f155c70
Nikhil Badyal <nikhill773384@gmail.com>
2022-05-12 07:44:27 +0530

    :sparkles: Added requestAllFilesPermission platform code

--- android/app/src/main/kotlin/com/nikhil/flutter/keepnotes/MainActivity.kt
+++ android/app/src/main/kotlin/com/nikhil/flutter/keepnotes/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.nikhil.flutter.keepnotes
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import android.util.Log
 import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 
 class MainActivity : FlutterFragmentActivity() {
+
+    companion object {
+        private const val INTENT_CHANNEL = "intentChannel"
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, INTENT_CHANNEL)
+                .setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
+                    when (call.method) {
+                        "requestAllFilesPermission" ->
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R &&
+                                !Environment.isExternalStorageManager()) {
+                                try {
+                                    val intent =
+                                    Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                                        .setData(Uri.parse("package:$packageName"))
+                                startActivity(intent)
+                                } catch (e: Exception) {
+                                   // failed to get permission
+                                }
+                            }
+                        else -> result.notImplemented()
+                    }
+    }}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
